33982ff5578aaf90aed66b7d17c7db847847ce53
Nguyen Tan Hung <hungofhydraforwork@gmail.com>
2022-07-15 15:35:09 +0700

    Implement JWT

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken');
+
+
+const authenticationMiddleware = async (req,res,next) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const {id, username} = decoded;
+        req.user = {id, username}
+        next()
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route', 401);
+    }
+  
+}
+
+module.exports = authenticationMiddleware
